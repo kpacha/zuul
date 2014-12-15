3314c68f43c6b737fd0bd448a4adeae7d0eded04
Kpacha <kpacha666@gmail.com>
2014-12-15 22:20:49 +0100

    extract the logic of the counter from the main method

--- zuul-simple-webapp/src/main/groovy/filters/pre/QoSFilter.groovy
+++ zuul-simple-webapp/src/main/groovy/filters/pre/QoSFilter.groovy
@@ -9,10 +9,12 @@ import redis.clients.jedis.JedisPoolConfig
  */
 class QoSFilter extends ZuulFilter {
 
+    private static final String HIGHT_PRIORITY_HEADER = "x-priority"
     private static final String HIGHT_PRIORITY_TOKEN = "abcdefghijklmnopqrstuvwxyz"
     private static final JedisPool JEDIS_POOL = new JedisPool(new JedisPoolConfig(), "localhost")
     private static final Long MAX_RATE = 10l
     private static final int RATE_TTL = 30
+    private static final int ERROR_CODE = 403
     private static final PATTERN = ~/\/(.*)\/.*/
 
     @Override
@@ -28,7 +30,7 @@ class QoSFilter extends ZuulFilter {
     @Override
     boolean shouldFilter() {
         return !HIGHT_PRIORITY_TOKEN.equals(
-            RequestContext.getCurrentContext().request.getHeader("X-Priority".toLowerCase())
+            RequestContext.getCurrentContext().request.getHeader(HIGHT_PRIORITY_HEADER)
         )
     }
 
@@ -40,30 +42,18 @@ class QoSFilter extends ZuulFilter {
         if(matcher.matches()){
             String tag = getTag(ctx, matcher[0][1])
 
-            Jedis jedis = JEDIS_POOL.getResource()
-            def rate = jedis.incr(tag)
-            if(1l == rate){
-                jedis.expire(tag, RATE_TTL)
-            }
-            ctx.addZuulResponseHeader("X-RateLimit-Tag", tag)
-            ctx.addZuulResponseHeader("X-RateLimit-Limit", String.valueOf(MAX_RATE))
-            ctx.addZuulResponseHeader("X-RateLimit-Remaining", String.valueOf(MAX_RATE - rate))
-            ctx.addZuulResponseHeader("X-RateLimit-TTL", String.valueOf(jedis.ttl(tag)))
-            if(rate >= MAX_RATE) {
-                ctx.setResponseStatusCode(403);
+            if(checkRate(ctx, tag) >= MAX_RATE) {
+                ctx.setResponseStatusCode(ERROR_CODE);
                 ctx.setResponseBody("{\"message\": \"API rate limit exceeded for [" + tag + "]. Please, leash your eager client!\"}")
                 ctx.setSendZuulResponse(false)
             }
-            jedis.close()
         }
 
     }
 
     private String getUri(RequestContext ctx) {
         String uri = ctx.request.getRequestURI()
-        if (ctx.requestURI != null) {
-            uri = ctx.requestURI
-        }
+        if (ctx.requestURI != null) uri = ctx.requestURI
         if (uri == null) uri = "/"
         uri
     }
@@ -78,6 +68,20 @@ class QoSFilter extends ZuulFilter {
         Map<String, String> zuulHeaders = ctx.getZuulRequestHeaders()
         resource + "-" + zuulHeaders.get("X-Api-User".toLowerCase()) + "-" +
             zuulHeaders.get("X-Api-Client".toLowerCase()) + "-" + getIpAddress(ctx)
+    }
+
+    private checkRate(RequestContext ctx, String tag) {
+        Jedis jedis = JEDIS_POOL.getResource()
+        def rate = jedis.incr(tag)
+        if(1l == rate) jedis.expire(tag, RATE_TTL)
+
+        ctx.addZuulResponseHeader("X-RateLimit-Tag", tag)
+        ctx.addZuulResponseHeader("X-RateLimit-Limit", String.valueOf(MAX_RATE))
+        ctx.addZuulResponseHeader("X-RateLimit-Remaining", String.valueOf(MAX_RATE - rate))
+        ctx.addZuulResponseHeader("X-RateLimit-TTL", String.valueOf(jedis.ttl(tag)))
+        jedis.close()
+
+        rate
     }
 
 }
